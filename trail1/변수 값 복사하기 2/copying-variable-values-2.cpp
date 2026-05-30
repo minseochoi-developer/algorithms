a7ad580aa3b1744f58ff9db54beb21675fcf8767
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 18:56:40 +0900

    [Passed] 변수 값 복사하기 2 (2ms, 0MB)

---
+++ trail1/변수 값 복사하기 2/copying-variable-values-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5, b = 6, c = 7;
+    a = b = c;
+
+    cout << a << " " << b << " " << c << endl;
+    return 0;
+}
