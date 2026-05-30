110a9efbfaa2b1a6f8c662c258ce4a76cbdaf59f
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 18:09:59 +0900

    [Passed] 소수점 반올림하기 (2ms, 0MB)

---
+++ trail1/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a = 25.352;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << a << endl;
+    
+    return 0;
+}
