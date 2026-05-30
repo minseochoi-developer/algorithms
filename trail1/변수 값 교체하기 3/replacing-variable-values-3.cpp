ca258571cc9a17327e39d92f4d2ff337854d8f51
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 18:17:41 +0900

    [Passed] 변수 값 교체하기 3 (2ms, 0MB)

---
+++ trail1/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3, b = 5;
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << a << "\n" << b << endl;
+    
+    return 0;
+}
