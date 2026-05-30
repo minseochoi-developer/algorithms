e133336738980a0ff09cc523ddb45ea0457d55e7
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 19:15:41 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printSentence(int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    printSentence(n);
+
+    return 0;
+}
