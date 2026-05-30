d3838e597824166e73ba95c43b5592a2e7e4c60a
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 18:58:59 +0900

    [Passed] 점수 입출력 (2ms, 0MB)

---
+++ trail1/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    cout << "Your score is " << n << " point." << endl;
+    return 0;
+}
