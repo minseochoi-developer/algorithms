26f535bb2b6bcf3ccef4f1e4e21d07385d0543a1
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 16:25:53 +0900

    [Passed] 정상적인 구구단 (2ms, 0MB)

---
+++ trail0/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (j == n)
+                cout << i << " * " << j << " = " << i * j;
+            else
+                cout << i << " * " << j << " = " << i * j << ", ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
