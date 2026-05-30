de3dcf2ff36a25a9db7cd605227d6a66fccf6341
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 18:04:16 +0900

    [Passed] 변수 선언하기 (2ms, 0MB)

---
+++ trail1/변수 선언하기/declaring-variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    char ch = 'C';
+
+    cout << a << "\n" << ch;
+
+    return 0;
+}
