9a01479a52342166f0be8582c8739f2fa9ab8b0a
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 18:16:55 +0900

    [Passed] 정수 복사 (2ms, 0MB)

---
+++ trail1/정수 복사/copy-integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3, b = 4;
+    b = a;
+    cout << a << " " << b << endl;
+    cout << a * b << endl;
+    
+    return 0;
+}
