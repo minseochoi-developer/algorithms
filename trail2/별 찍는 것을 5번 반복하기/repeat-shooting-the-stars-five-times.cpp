4f82c6cf6e0c3f613623c64ff02f72364afc4cd4
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 19:13:22 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printStars() {
+    for (int i = 0; i < 10; i++) {
+        cout << "*";
+    }
+    cout << endl;
+}
+
+int main() {
+    // Please write your code here.
+    for (int i = 0; i < 5; i++) {
+        printStars();
+    }
+    
+    return 0;
+}
