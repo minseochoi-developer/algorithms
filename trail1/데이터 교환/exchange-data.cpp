9ea855843fd6c1e327d106121c94e0cf42da099b
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 18:53:50 +0900

    [Passed] 데이터 교환 (2ms, 0MB)

---
+++ trail1/데이터 교환/exchange-data.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5, b = 6, c = 7;
+
+    int temp = b;
+    b = a;
+    a = c;
+    c = temp;
+
+    cout << a << "\n" << b << "\n" << c << endl;
+
+    return 0;
+}
