7a5e87f2b55c114e66953ff0d1934d2ec8572a2b
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 15:43:51 +0900

    [Passed] 짝수만 거꾸로 출력하기 (2ms, 0MB)

---
+++ trail0/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int num[n];
+    for (int i = 0; i < n; i++) {
+        cin >> num[i];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (num[i] % 2 == 0)
+            cout << num[i] << " ";
+    }
+    
+    return 0;
+}
