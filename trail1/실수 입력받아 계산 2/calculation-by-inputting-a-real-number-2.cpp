33da6a387733002a29ba3cc1d03ce1a0467bf002
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 19:02:22 +0900

    [Passed] 실수 입력받아 계산 2 (2ms, 0MB)

---
+++ trail1/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a;
+    cin >> a;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << a + 1.5 << endl;
+    return 0;
+}
