c49173c30a00de777a140bcd20f812bd72f55ef5
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 19:01:34 +0900

    [Passed] 길이 단위 환산하기 (2ms, 0MB)

---
+++ trail1/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double n;
+    cin >> n;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << n * 30.48 << endl;
+    return 0;
+}
