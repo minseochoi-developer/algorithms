633e017f07549b51111c78ddcdaef123f79957b1
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-05-30 15:37:45 +0900

    [Passed] a로 채워넣기 (2ms, 0MB)

---
+++ trail0/a로 채워넣기/filling-with-a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+
+    str[1] = 'a';
+    str[str.length() - 2] = 'a';
+
+    cout << str << endl;
+    
+    return 0;
+}
