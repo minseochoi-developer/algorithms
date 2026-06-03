92735759b633fe9a994b804a8bf64cce043edccf
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-03 19:42:36 +0900

    [Passed] 2021년 날짜의 유무 (2ms, 0MB)

--- trail2/2021년 날짜의 유무/with-or-without-2021.cpp
+++ trail2/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -1,48 +1,30 @@
 #include <iostream>
+
 using namespace std;
 
-bool isDay(int month, int day) {
-    if (month <= 7) {
-        if (month % 2 == 0) {
-            if (month == 2) {
-                if (day <= 28)
-                    return true;
-                else
-                    return false;
-            } else {
-                if (day <= 30)
-                    return true;
-                else
-                    return false;
-            }
-        } else {
-            if (day <= 31)
-                return true;
-            else
-                return false;
-        }
-    } else if (month <= 12) {
-        if (month % 2 == 0)
-            if (day <= 31)
-                return true;
-            else
-                return false;
-        else
-            if (day <= 30)
-                return true;
-            else
-                return false;
-    }
+int M, D;
+
+int getLastDay() {
+    if (M == 2)
+        return 28;
+    else if (M == 4 || M == 6 || M == 9 || M == 11)
+        return 30;
+    
+    return 31;
+}
+
+bool isDay() {
+    if (M <= 12 && D <= getLastDay())
+        return true;
     
     return false;
 }
 
 int main() {
-    // Please write your code here.
-    int m, d;
-    cin >> m >> d;
+    cin >> M >> D;
 
-    if (isDay(m, d))
+    // Please write your code here.
+    if (isDay())
         cout << "Yes" << endl;
     else
         cout << "No" << endl;
