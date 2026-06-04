931c3d1cede859b91f13da0e863eafbe9bb363b5
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-04 19:41:22 +0900

    [Passed] 중앙값 계산 2 (2ms, 0MB)

---
+++ trail2/중앙값 계산 2/get-median-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#define MAX_N 100
+
+using namespace std;
+
+int arr[MAX_N];
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+
+        if ((i + 1) % 2 == 1) {
+            sort(arr, arr + i + 1);
+            cout << arr[i / 2] << " ";
+        }
+    }
+
+    return 0;
+}
