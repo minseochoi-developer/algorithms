5752cdba764194f6dc90e5f8d709c084d22a19ed
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-04 14:46:35 +0900

    [Passed] 오름 내림차순 정렬 (2ms, 0MB)

---
+++ trail2/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    sort(arr, arr + n, greater<int>());
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
