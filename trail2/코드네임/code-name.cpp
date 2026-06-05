c04286cfc60d3368551daf62cb148513a848d878
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-06 00:05:23 +0900

    [Passed] 코드네임 (2ms, 0MB)

---
+++ trail2/코드네임/code-name.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Member {
+    public:
+        char code_name;
+        int score;
+
+    Member(char code_name, int score) {
+        this->code_name = code_name;
+        this->score = score;
+    }
+
+    Member() {};
+};
+
+int main() {
+    // Please write your code here.
+    Member members[5];
+    for (int i = 0; i < 5; i++) {
+        char code_name;
+        int score;
+        cin >> code_name >> score;
+        members[i] = Member(code_name, score);
+    }
+
+    char name;
+    int min_score = 101;
+    for (int i = 0; i < 5; i++) {
+        if (members[i].score < min_score) {
+            name = members[i].code_name;
+            min_score = members[i].score;
+        }
+    }
+
+    cout << name << " " << min_score << endl;
+    return 0;
+}
