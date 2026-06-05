ff1230504d568c5488856c439d655fe262a618b7
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-05 23:48:34 +0900

    [Passed] Next Level (2ms, 0MB)

--- trail2/Next Level/next-level.cpp
+++ trail2/Next Level/next-level.cpp
@@ -3,23 +3,12 @@
 
 using namespace std;
 
-class DefaultInfo {
+class User {
     public:
         string id;
         int level;
 
-    DefaultInfo(string id = "codetree", int level = 10) {
-        this->id = id;
-        this->level = level;
-    }
-};
-
-class Info {
-    public:
-        string id;
-        int level;
-
-    Info(string id, int level) {
+    User(string id = "codetree", int level = 10) {
         this->id = id;
         this->level = level;
     }
@@ -27,15 +16,18 @@ class Info {
 
 int main() {
     // Please write your code here.
-    DefaultInfo defaultInfo = DefaultInfo();
+    User user1 = User();
     
-    string id;
-    int level;
-    cin >> id >> level;
-    Info info = Info(id, level);
+    string id2;
+    int level2;
+    cin >> id2 >> level2;
 
-    cout << "user " << defaultInfo.id << " lv " << defaultInfo.level << endl;
-    cout << "user " << info.id << " lv " << info.level << endl;
+    User user2 = User();
+    user2.id = id2;
+    user2.level = level2;
+
+    cout << "user " << user1.id << " lv " << user1.level << endl;
+    cout << "user " << user2.id << " lv " << user2.level << endl;
 
     return 0;
 }
