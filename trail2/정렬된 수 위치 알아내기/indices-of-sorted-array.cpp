68e5bf8362aa5a90d0f48a5b23df633d4ce31e40
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-10 17:46:52 +0900

    [Passed] 정렬된 수 위치 알아내기 (2ms, 0MB)

--- trail2/정렬된 수 위치 알아내기/indices-of-sorted-array.cpp
+++ trail2/정렬된 수 위치 알아내기/indices-of-sorted-array.cpp
@@ -1,40 +1,32 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-#include <tuple>
 
 #define MAX_N 1000
 
 using namespace std;
 
-bool Cmp(tuple<int, int, int> a, tuple<int, int, int> b) {
-    if (get<0>(a) == get<0>(b))
-        return get<1>(a) < get<1>(b);
-    return get<0>(a) < get<0>(b);
-}
-
-tuple<int, int, int> seq[MAX_N];
-
 int main() {
     // Please write your code here.
-    int n;
+    int n, num_cache;
+    int answer[MAX_N];
+    vector<pair<int, int>> numbers;
+
     cin >> n;
     for (int i = 0; i < n; i++) {
-        int num;
-        cin >> num;
-        seq[i] = make_tuple(num, i + 1, 0);
+        cin >> num_cache;
+        numbers.push_back(make_pair(num_cache, i));
     }
 
-    sort(seq, seq + n, Cmp);
+    sort(numbers.begin(), numbers.end());
 
     for (int i = 0; i < n; i++) {
-        int ori_idx = get<1>(seq[i]) - 1;
-        int target_num, target_ori;
-        tie(target_num, target_ori, ignore) = seq[ori_idx];
-        seq[ori_idx] = make_tuple(target_num, target_ori, i + 1);
+        answer[numbers[i].second] = i + 1;
     }
 
     for (int i = 0; i < n; i++) {
-        cout << get<2>(seq[i]) << " ";
+        cout << answer[i] << " ";
     }
+
     return 0;
 }
