b65739204df5b82485056a18169bba18e656bf5a
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-10 22:08:38 +0900

    [Passed] Date to Date (2ms, 0MB)

--- trail2/Date to Date/date-to-date.cpp
+++ trail2/Date to Date/date-to-date.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int NumOfDays(int m, int d) {
+    int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int total_days = 0;
+
+    for (int i = 1; i < m; i++) {
+        total_days += num_of_days[i];
+    }
+
+    total_days += d;
+
+    return total_days;
+}
+
 int main() {
     // Please write your code here.
     int m1, d1, m2, d2;
     cin >> m1 >> d1 >> m2 >> d2;
 
-    if (m1 == m2) {
-        cout << d2 - d1 + 1 << endl;
-        return 0;
-    }
-
-    int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int elapsed_days = 0;
-
-    for (int i = m1; i <= m2; i++) {
-        if (i == m1)
-            elapsed_days += num_of_days[i] - d1 + 1;
-        else if (i == m2)
-            elapsed_days += d2;
-        else
-            elapsed_days += num_of_days[i];
-    }
-
-    cout << elapsed_days << endl;
+    cout << NumOfDays(m2, d2) - NumOfDays(m1, d1) + 1<< endl;
 
     return 0;
 }
