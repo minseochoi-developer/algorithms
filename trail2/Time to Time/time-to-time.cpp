15caf6168888482c24805c7cba1bac2e3b900520
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-10 21:42:41 +0900

    [Passed] Time to Time (2ms, 0MB)

--- trail2/Time to Time/time-to-time.cpp
+++ trail2/Time to Time/time-to-time.cpp
@@ -4,10 +4,8 @@ using namespace std;
 int main() {
     // Please write your code here.
     int hour_a, minute_a, hour_b, minute_b;
-    cin >> hour_a >> minute_a;
-    cin >> hour_b >> minute_b;
-
-    int time = (hour_b * 60 + minute_b) - (hour_a * 60 + minute_a);
-    cout << time << endl;
+    cin >> hour_a >> minute_a >> hour_b >> minute_b;
+    
+    cout << (hour_b * 60 + minute_b) - (hour_a * 60 + minute_a) << endl;
     return 0;
 }
