e485a08b71848042408f90e5ff7017904f56d798
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-12 15:25:07 +0900

    [Passed] 십진수로 변환하기 (2ms, 0MB)

--- trail2/십진수로 변환하기/convert-to-decimal.cpp
+++ trail2/십진수로 변환하기/convert-to-decimal.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    // Please write your code here.
-    int digit, num = 0;
-    int digits[8], cnt = 0;
-
-    cin >> digit;
-
-    while (digit != 0) {
-        digits[cnt++] = digit % 10;
-        digit /= 10;
-    }
-
-    for (int i = cnt - 1; i >= 0; i--) {
-        num = num * 2 + digits[i];
-    }
-
-    cout << num << endl;
-
+    string binary;
+    cin >> binary;
+    
+    int num = 0;
+    for(int i = 0; i < (int) binary.size(); i++)
+        num = num * 2 + (binary[i] - '0');
+    
+    cout << num;
     return 0;
 }
