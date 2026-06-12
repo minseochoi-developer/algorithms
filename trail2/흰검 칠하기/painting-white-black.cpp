128d83ff650ff97e33f7831912b679dd735b8104
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-12 18:32:10 +0900

    [Passed] 흰검 칠하기 (3ms, 1MB)

--- trail2/흰검 칠하기/painting-white-black.cpp
+++ trail2/흰검 칠하기/painting-white-black.cpp
@@ -1,65 +1,46 @@
 #include <iostream>
 
-#define MAX_TILE 200000
-#define OFFSET 100000
 using namespace std;
 
+#define MAX_K 100000
+
+int n;
+int a[2 * MAX_K + 1];
+int cnt_b[2 * MAX_K + 1];
+int cnt_w[2 * MAX_K + 1];
+int b, w, g;
+
 int main() {
-    // Please write your code here.
-    int n;
     cin >> n;
 
-    int white[MAX_TILE + 1] = {0, };
-    int black[MAX_TILE + 1] = {0, };
-    char color[MAX_TILE + 1] = {'N', }; 
-    int cur = 0;
+    int cur = MAX_K;
 
-    for (int i = 0; i < n; i++) {
-        int dist;
-        char dic;
-        cin >> dist >> dic;
-
-        int x1, x2;
-        if (dic == 'L') {
-            x1 = cur - dist + 1;
-            x2 = cur;
-            cur = cur - dist + 1;
-        } else {
-            x1 = cur;
-            x2 = cur + dist - 1;
-            cur = cur + dist - 1;
-        }
-
-        x1 += OFFSET;
-        x2 += OFFSET;
-
-        for (int j = x1; j <= x2; j++) {
-            if (color[j] == 'G')
-                continue;
-            
-            if (dic == 'L') {
-                white[j]++;
-                color[j] = 'W';
-            } else {
-                black[j]++;
-                color[j] = 'B';
+    for(int i = 1; i <= n; i++) {
+        int x;
+        char c;
+        cin >> x >> c;
+        if(c == 'L') {
+            while(x--) {
+                a[cur] = 1;
+                cnt_w[cur]++;
+                if(x) cur--;
             }
-
-            if (white[j] >= 2 && black[j] >= 2)
-                color[j] = 'G';
+        }
+        else {
+            while(x--) {
+                a[cur] = 2;
+                cnt_b[cur]++;
+                if(x) cur++;
+            }
         }
     }
 
-    int w = 0, b = 0, g = 0;
-    for (int i = 0; i <= MAX_TILE; i++) {
-        if (color[i] == 'W')
-            w++;
-        else if (color[i] == 'B') 
-            b++;
-        else if (color[i] == 'G')
-            g++;
+    for(int i = 0; i <= 2 * MAX_K; i++) {
+        if(cnt_b[i] >= 2 && cnt_w[i] >= 2) g++;
+        else if(a[i] == 1) w++;
+        else if(a[i] == 2) b++;
     }
 
-    cout << w << " " << b << " " << g << endl;
+    cout << w << " " << b << " " << g;
     return 0;
 }
