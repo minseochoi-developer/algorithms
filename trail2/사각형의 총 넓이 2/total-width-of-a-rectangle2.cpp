62f36161d97a1915764536e9a0910f6aeea3e14b
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-12 18:58:30 +0900

    [Passed] 사각형의 총 넓이 2 (2ms, 1MB)

--- trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -17,6 +17,8 @@ int main() {
 
         x1 += OFFSET;
         x2 += OFFSET;
+        y1 += OFFSET;
+        y2 += OFFSET;
 
         for (int x = x1; x < x2; x++) {
             for (int y = y1; y < y2; y++) {
