fd61362ef911b3ea4a2b84cd14baf3ce41b1908a
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-15 15:05:12 +0900

    [Passed] 계속 중첩되는 사각형 (2ms, 0MB)

---
+++ trail2/계속 중첩되는 사각형/continuously-overlapping-squares.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#define MAX_R 200
+#define OFFSET 100
+
+using namespace std;
+
+int checked[MAX_R + 1][MAX_R + 1];
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        x1 += OFFSET; x2 += OFFSET; y1 += OFFSET; y2 += OFFSET;
+
+        for (int x = x1; x < x2; x++) {
+            for (int y = y1; y < y2; y++) {
+                checked[x][y] = i % 2 + 1;
+            }
+        }
+    }
+
+    int area = 0;
+    for (int x = 0; x <= MAX_R; x++) {
+        for (int y = 0; y <= MAX_R; y++) {
+            if (checked[x][y] == 2)
+                area++;
+        }
+    }
+
+    cout << area << endl;
+
+    return 0;
+}
