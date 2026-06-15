76ad1f9f02cb65f8b00f70fa210ad7bfa85f9791
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-15 16:56:05 +0900

    [Passed] 좌우로 움직이는 로봇 (54ms, 16MB)

---
+++ trail2/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+#define MAX_DIS 2000000
+
+using namespace std;
+
+int n, m;
+int robot_a[MAX_DIS + 1], robot_b[MAX_DIS + 1];
+
+int main() {
+    // Please write your code here.
+    cin >> n >> m;
+
+    int time_a = 1;
+    for (int i = 0; i < n; i++) {
+        int t;
+        char d;
+        cin >> t >> d;
+
+        while (t--) {
+            if (d == 'L')
+                robot_a[time_a] = robot_a[time_a - 1] - 1;
+            else
+                robot_a[time_a] = robot_a[time_a - 1] + 1;
+            
+            time_a++;
+        }
+    }
+
+    int time_b = 1;
+    for (int i = 0; i < m; i++) {
+        int t;
+        char d;
+        cin >> t >> d;
+
+        while (t--) {
+            if (d == 'L')
+                robot_b[time_b] = robot_b[time_b - 1] - 1;
+            else
+                robot_b[time_b] = robot_b[time_b - 1] + 1;
+            
+            time_b++;
+        }
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= MAX_DIS; i++) {
+        if (i >= time_a)
+            robot_a[i] = robot_a[time_a - 1];
+        
+        if (i >= time_b)
+            robot_b[i] = robot_b[time_b - 1];
+        
+        if (robot_a[i] == robot_b[i] && robot_a[i - 1] != robot_b[i - 1])
+            ans++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
