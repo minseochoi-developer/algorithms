d5ec1ce40b3a501bb2fed13487605fc071ccd7d6
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-15 13:48:13 +0900

    [Passed] 겹치지 않는 사각형의 넓이 (24ms, 16MB)

--- trail2/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
+++ trail2/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -1,51 +1,44 @@
 #include <iostream>
 
+#define N 3
+#define MAX_R 2000
 #define OFFSET 1000
 
 using namespace std;
 
-int rec[OFFSET * 2 + 1][OFFSET * 2 + 1];
+int x1[N], y1[N];
+int x2[N], y2[N];
+
+int checked[MAX_R + 1][MAX_R + 1];
 
 int main() {
     // Please write your code here.
-    int x1, y1, x2, y2;
-    int area = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
 
-    for (int i = 0; i < 2; i++) {
-        cin >> x1 >> y1 >> x2 >> y2;
+        x1[i] += OFFSET;
+        y1[i] += OFFSET;
+        x2[i] += OFFSET;
+        y2[i] += OFFSET;
+    }
 
-        x1 += OFFSET;
-        y1 += OFFSET;
-        x2 += OFFSET;
-        y2 += OFFSET;
-
-        for (int x = x1; x < x2; x++) {
-            for (int y = y1; y < y2; y++) {
-                rec[x][y] = 1;
+    for (int i = 0; i < N; i++) {
+        for (int x = x1[i]; x < x2[i]; x++) {
+            for (int y = y1[i]; y < y2[i]; y++) {
+                checked[x][y] = i + 1;
             }
         }
     }
 
-    cin >> x1 >> y1 >> x2 >> y2;
-
-    x1 += OFFSET;
-    y1 += OFFSET;
-    x2 += OFFSET;
-    y2 += OFFSET;
-
-    for (int x = x1; x < x2; x++) {
-        for (int y = y1; y < y2; y++) {
-            rec[x][y] = 0;
-        }
-    }
-
-    for (int x = 0; x < OFFSET * 2 + 1; x++) {
-        for (int y = 0; y < OFFSET * 2 + 1; y++) {
-            area += rec[x][y];
+    int area = 0;
+    for (int x = 0; x <= MAX_R; x++) {
+        for (int y = 0; y <= MAX_R; y++) {
+            if (checked[x][y] == 1 || checked[x][y] == 2) {
+                area++;
+            }
         }
     }
 
     cout << area << endl;
-
     return 0;
 }
