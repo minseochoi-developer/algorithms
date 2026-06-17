9e745b30ac6a005102a9a8439fb2cbe94580bbfe
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-06-17 14:58:09 +0900

    [Passed] 빙빙 돌며 숫자 사각형 채우기 2 (2ms, 0MB)

---
+++ trail2/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#define MAX_R 100
+
+using namespace std;
+
+int n, m;
+int grid[MAX_R][MAX_R];
+int x, y, dir_num;
+
+bool IsFill(int x, int y) {
+    return (grid[x][y] != 0);
+}
+
+bool InRange(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < m;
+}
+
+void Simulate() {
+    int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+    int nx = x + dx[dir_num], ny = y + dy[dir_num];
+
+    if (!InRange(nx, ny) || IsFill(nx, ny))
+        dir_num = (dir_num + 1) % 4;
+    
+    x = x + dx[dir_num], y = y + dy[dir_num];
+}
+
+int main() {
+    // Please write your code here.
+    cin >> n >> m;
+
+    for (int num = 1; num <= n * m; num++) {
+        grid[x][y] = num;
+        Simulate();
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
