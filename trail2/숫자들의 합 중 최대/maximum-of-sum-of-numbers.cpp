5f8bd7a4df766f6279ef76985d7802e039bcfdd8
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-07-19 14:54:20 +0900

    [Passed] 숫자들의 합 중 최대 (2ms, 0MB)

--- trail2/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
+++ trail2/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
@@ -3,21 +3,23 @@
 
 using namespace std;
 
+int x, y;
+
+int DigitSum(int n) {
+    if (n < 10)
+        return n;
+    else
+        return DigitSum(n / 10) + n % 10;
+}
+
 int main() {
     // Please write your code here.
-    int x, y;
     cin >> x >> y;
 
     int max_num = 0;
-    for (int i = x; i <= y; i++) {
-        int d1 = i / 10000;
-        int d2 = (i % 10000) / 1000;
-        int d3 = (i % 1000) / 100;
-        int d4 = (i % 100) / 10;
-        int d5 = i % 10;
 
-        int sum = d1 + d2 + d3 + d4 + d5;
-        max_num = max(max_num, sum);
+    for (int i = x; i <= y; i++) {
+        max_num = max(max_num, DigitSum(i));
     }
 
     cout << max_num << endl;
