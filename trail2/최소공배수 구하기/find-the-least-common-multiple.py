97b881f67a10c65de739a187014a1dc55f1b06c6
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-07-28 19:29:37 +0900

    [Passed] 최소공배수 구하기 (51ms, 16MB)

---
+++ trail2/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,11 @@
+n, m = tuple(map(int, input().split()))
+
+def find_lcm(n, m):
+    gcd = 0
+    for i in range(1, min(n, m) + 1):
+        if n % i == 0 and m % i == 0:
+            gcd = i
+    
+    print (n * m // gcd)
+
+find_lcm(n, m)
