df04712ac7d955623762591dbe4640ebb580fc07
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-07-28 21:34:45 +0900

    [Passed] 절댓값으로 변경 (47ms, 16MB)

---
+++ trail2/절댓값으로 변경/find-the-absolute-value.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+def modify(arr):
+    for i in range(n):
+        if (arr[i] < 0):
+            arr[i] = -arr[i]
+
+
+modify(arr)
+for i in range(n):
+    print(arr[i], end=" ")
