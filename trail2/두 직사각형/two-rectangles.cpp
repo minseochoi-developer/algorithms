c22b7877358214b9f0b1f3938d16766cc222d572
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-07-28 14:09:21 +0900

    [Passed] 두 직사각형 (2ms, 0MB)

---
+++ trail2/두 직사각형/two-rectangles.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int x1, y1, x2, y2, a1, b1, a2, b2;
+    cin >> x1 >> y1 >> x2 >> y2 >> a1 >> b1 >> a2 >> b2;
+
+    if (x2 < a1 || a2 < x1 || b2 < y1 || y2 < b1)
+        cout << "nonoverlapping";
+    else
+        cout << "overlapping";
+        
+    return 0;
+}
