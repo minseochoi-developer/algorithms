ee6ef7e25ff41790b4b9235d057ad8198a3c86a5
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-07-28 20:33:13 +0900

    [Passed] 함수를 이용한 온전수 판별 (53ms, 16MB)

---
+++ trail2/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,20 @@
+a, b = tuple(map(int, input().split()))
+
+def is_onjeonsu(num):
+    if (num % 2 == 0):
+        return False
+    
+    if (num % 10 == 5):
+        return False
+    
+    if (num % 3 == 0 and num % 9 != 0):
+        return False
+    
+    return True
+
+cnt = 0
+for i in range(a, b+1):
+    if (is_onjeonsu(i)):
+        cnt += 1
+
+print(cnt)
