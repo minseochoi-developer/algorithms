9a6f92337f38f0248bc2893e170ec4ac1907a697
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-07-29 00:46:51 +0900

    [Passed] 2개씩 그룹짓기 (54ms, 17MB)

---
+++ trail2/2개씩 그룹짓기/group-of-pairs.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+arr.sort()
+
+group_max = 0
+for i in range(n):
+    group_sum = arr[i] + arr[2*n - 1 - i]
+    if group_sum > group_max:
+        group_max = group_sum
+
+print(group_max)
