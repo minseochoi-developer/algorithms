d5c3c6dc819543a665ca017cf723cbb862ccbca2
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-07-28 20:53:36 +0900

    [Passed] 함수를 이용한 연속부분수열 여부 판단하기 (52ms, 16MB)

---
+++ trail2/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,20 @@
+n1, n2 = tuple(map(int, input().split()))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+def is_same(n):
+    for i in range(n2):
+        if a[i + n] != b[i]:
+            return False
+    return True
+
+def is_subsequence():
+    for i in range(n1 - n2 + 1):
+        if is_same(i):
+            return True
+    return False
+
+if is_subsequence():
+    print("Yes")
+else:
+    print("No")
