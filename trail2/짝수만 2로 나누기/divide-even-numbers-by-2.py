6ced06f47722c54d8ff97107fb5e0848eaf5f02a
choiminseo <161548105+minseochoi-developer@users.noreply.github.com>
2026-07-28 21:17:18 +0900

    [Passed] 짝수만 2로 나누기 (51ms, 16MB)

---
+++ trail2/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+def even_divide(arr):
+    for i in range(n):
+        if (arr[i] % 2 == 0):
+            arr[i] //= 2
+
+even_divide(arr)
+for elem in arr:
+    print(elem, end=" ")
